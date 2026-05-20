40589bea83b23f9ab6c2b62cdf5dba9298f1239a
AndreLuiz-Cardoso <andre.lc93@hotmail.com>
2026-05-20 16:41:02 +0200

    test

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --format=Raw-SHA256 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --show --format=Raw-SHA256 "$1" | grep "^?:" | cut -d: -f2 > 6-password.txt
+john --show --format=Raw-SHA256 "$1" | grep -Eo '[a-f0-9]+:.+' | cut -d: -f2 > 6-password.txt

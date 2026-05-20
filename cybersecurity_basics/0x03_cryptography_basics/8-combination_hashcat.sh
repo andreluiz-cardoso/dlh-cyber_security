2f1bca51447d79c74e9be03d9ef17a4ed36fdcd5
AndreLuiz-Cardoso <andre.lc93@hotmail.com>
2026-05-20 16:55:29 +0200

    test

--- cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --format=Raw-SHA256 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --show --format=Raw-SHA256 "$1" | grep "^?:" | cut -d: -f2 > 6-password.txt
+hashcat -a 1 --stdout "$1" "$2"

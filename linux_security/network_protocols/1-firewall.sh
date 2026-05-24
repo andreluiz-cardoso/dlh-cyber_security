b41e6594a738990be085392896154a8012b9ae21
AndreLuiz-Cardoso <andre.lc93@hotmail.com>
2026-05-24 18:49:58 +0200

    1 firewall fixed

--- linux_security/network_protocols/1-firewall.sh
+++ linux_security/network_protocols/1-firewall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-iptables -P INPUT DROP
-iptables -A INPUT -i lo -j ACCEPT; iptables -A INPUT -n state --state ESTABLISHED,RELATED -j ACCEPT; iptables
--A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -P INPUT DROP
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
